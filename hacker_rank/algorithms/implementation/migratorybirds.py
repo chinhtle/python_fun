a2f4ed80657fb5c887959fe40dca275469275df0
Chinh Le <chinh.t.le@gmail.com>
2017-03-15 15:11:05 -0700

    Hackerrank algorithms implementation - migratory birds
    Given an array of n integers where each integer describes the
    type of a bird in the flock, find and print the type number of
    the most common bird. If two or more types of birds are equally
    common, choose the type with the smallest ID number.

---
+++ hacker_rank/algorithms/implementation/migratorybirds.py
@@ -0,0 +1,56 @@
+# A flock of n birds is flying across the continent. Each bird has a type, and
+# the different types are designated by the ID numbers 1, 2, 3, 4, and 5.
+#
+# Given an array of n integers where each integer describes the type of a bird
+# in the flock, find and print the type number of the most common bird. If two
+# or more types of birds are equally common, choose the type with the smallest
+# ID number.
+#
+# Input Format:
+# The first line contains an integer denoting n (the number of birds).
+# The second line contains n space-separated integers describing the respective
+# type numbers of each bird in the flock.
+#
+# Constraints:
+#   5 <= n <= 2 * 10^5
+#   It is guaranteed that each type is 1, 2, 3, 4, or 5.
+#
+# Output Format:
+# Print the type number of the most common bird; if two or more types of birds
+# are equally common, choose the type with the smallest ID number.
+#
+# Sample Input 0:
+# 6
+# 1 4 4 4 5 3
+#
+# Sample Output 0:
+# 4
+#
+# Explanation 0:
+# The different types of birds occur in the following frequencies:
+#     Type 1: 1 bird
+#     Type 2: 0 birds
+#     Type 3: 1 bird
+#     Type 4: 3 birds
+#     Type 5: 1 bird
+#
+# The type number that occurs at the highest frequency is type 4, so we print
+# 4 as our answer.
+
+_ = raw_input()  # Not used
+types = map(int, raw_input().split())
+type_count = [0] * 6  # 0 (not used), 1-5 bird types
+
+bird_type = max_birds = 0
+
+for t in types:
+    type_count[t] += 1
+
+    if type_count[t] > max_birds:
+        max_birds = type_count[t]
+        bird_type = t
+    elif type_count[t] == max_birds:
+        if t < bird_type:
+            bird_type = t
+
+print bird_type
